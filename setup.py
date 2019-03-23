9d2f0c6a15c104b645bb66fcfb4f10d3ddbc20b7
hilmunga <hilmunga@gmail.com>
2019-03-23 03:29:40 +0300

    abilityhemu_blog

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='abilityhemu',
+    version='V2019',
+    packages=['abilityhemu', 'abilityhemu_blog', 'abilityhemu_blog.migrations'],
+    url='https://www.abilityhemu.tech',
+    license='GPL',
+    author='Hilmunga',
+    author_email='hilmunga@gmail.com',
+    description='Blog'
+)
